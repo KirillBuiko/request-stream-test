9ed14580ac795596a2854b842a970b0770210e2a
KirillBuiko <k.buiko04@mail.ru>
2023-12-04 04:50:54 +0700

    Add empty swagger

--- src/grpc-stream-redirect-test/core/servers/RestApiServer.ts
+++ src/grpc-stream-redirect-test/core/servers/RestApiServer.ts
@@ -2,6 +2,8 @@ import type {IServer, ServerStatus} from "@src/grpc-stream-redirect-test/core/ap
 import type {ServerConfigType} from "@src/grpc-stream-redirect-test/core/app/types/ServerConfigType";
 import Fastify, {RouteHandlerMethod} from "fastify";
 import {fastifyMultipart} from "@fastify/multipart";
+import swagger from "swagger-ui-dist";
+import fStat from "@fastify/static"
 
 export class RestApiServer implements IServer{
     status: ServerStatus = "off";
@@ -13,18 +15,31 @@ export class RestApiServer implements IServer{
                 fileSize: 1024 * 1024 * 1024
             }
         });
+        console.log(swagger.absolutePath());
+        this.server.register(fStat, {
+            root: swagger.absolutePath(),
+            // prefix: "/swagger",
+            logLevel: "info"
+        })
 
-        this.server.post('/get', this.getHandler);
+        this.server.get('/get', this.getHandler);
         this.server.post('/set', this.setHandler);
     }
 
-    start(config: ServerConfigType): Promise<Error | null> {
+    async start(config: ServerConfigType): Promise<Error | null> {
         this.status = "on";
-        return Promise.resolve(null);
+        await this.server.listen({
+            port: config.port
+        }, (err) => {
+            if(err)
+            console.log(err);
+        });
+        return null;
     }
 
     stop(): Promise<Error | undefined> {
         this.status = "off";
+        this.server.close();
         return Promise.resolve(undefined);
     }
 
